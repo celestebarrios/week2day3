27e73dc42b447acf7b1a2b5025fd2e35c5a1fcf7
celestebarrios <celestebarrios35@gmail.com>
2020-05-14 09:27:59 -0500

    Homework

---
+++ modulehw.py
@@ -0,0 +1,15 @@
+def printName(name):
+    print(f"hello Mr/Ms {name} ...we've been waiting for you")
+    
+my_own_pi=3.14
+
+
+
+def area(length,width):
+    area =length *width 
+    print(f"the area is {}area)
+area(10,5)
+def circumference(diameter):
+          circumference=2*diameter*3.14 
+          print(f"the circumference of the house)
+circumference()
